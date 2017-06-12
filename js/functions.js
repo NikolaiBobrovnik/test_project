a1f25587daa267db73e3a3bca338a2d12eec15f0
nick_437 <nick_437@mail.ru>
2017-06-12 12:09:38 +0300

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+
+
+    var containerHeader = $('.header').height();
+
+    $(window).scroll(function () {
+
+        var wScroll = $(this).scrollTop();
+
+        if (wScroll <= containerHeader) {
+
+            $('.header-box').css({
+                'transform': 'translate(0px, -' + wScroll / 4 + '%)'
+            });
+
+        }
+
+        if (wScroll > $('.about-us-wrapper').offset().top - $(window).height()) {
+
+            var opacity = (wScroll - $('.about-us-wrapper').offset().top + 500) / (wScroll / 4);
+
+            $('.about-us').css({'opacity': opacity});
+
+        }
+
+
+        if (wScroll > $('.our-team').offset().top - ($(window).height() / 1.2)) {
+
+            $('.our-team figure').each(function (i) {
+
+                setTimeout(function () {
+                    $('.our-team figure').eq(i).addClass('is-showing');
+                }, (700 * (Math.exp(i * 0.3))) - 700);
+
+            })
+
+        }
+
+        if (wScroll > $('.our-skills').offset().top - ($(window).height() / 1.2)) {
+
+            $('.our-skills figure').each(function (i) {
+
+                setTimeout(function () {
+                    $('.our-skills figure').eq(i).addClass('is-showing');
+                }, (1000 * (Math.exp(i * 0.25))) - 1000);
+
+            });
+
+        }
+
+    });
+});
+
+
+//skills
+
+var options = {
+    segmentShowStroke: false,
+    percentageInnerCutout: 84,
+    //animationIterationCount: 1,
+    //animation: true,
+    animationEasing: 'easeOutQuint',
+    //animateRotate: false,
+    //animateScale: true
+};
+var data = {
+    html: [{
+        value: 75,
+        color: "rgb(255, 0, 0)"
+    }, {
+        value: 25,
+        color: "rgb(218, 218, 218)"
+    }],
+    css: [{
+        value: 80,
+        color: "rgb(255, 0, 0)"
+    }, {
+        value: 20,
+        color: "rgb(218, 218, 218)"
+    }],
+    wordpress: [{
+        value: 70,
+        color: "rgb(255, 0, 0)"
+    }, {
+        value: 30,
+        color: "rgb(218, 218, 218)"
+    }],
+    seo: [{
+        value: 85,
+        color: "rgb(255, 0, 0)"
+    }, {
+        value: 15,
+        color: "rgb(218, 218, 218)"
+    }]
+};
+
+var offset = 3000;
+$.each(data, function (key, data) {
+    var canvas = document.querySelector('#' + key);
+    if (canvas) {
+        offset += 500;
+        setTimeout(function () {
+            var ctx = canvas.getContext('2d');
+            var chart = new Chart(ctx);
+            chart.Doughnut(data, options);
+        }, offset);
+    }
+});
